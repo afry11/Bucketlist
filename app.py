f6e9c89175cf05790a4c3432ff530ced0e64d669
dafigemink <alhaqdafi709@gmail.com>
2023-06-08 09:58:17 -0700

    first commit

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
 
 from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get(
-    "mongodb+srv://afriyanto123:afriyanto11@cluster0.pbnkpk6.mongodb.net/?retryWrites=true&w=majority"
-)
-DB_NAME = os.environ.get("dbsparta")
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+client = MongoClient(MONGODB_URI)
 
-client = MongoClient(
-    "mongodb+srv://afriyanto123:afriyanto11@cluster0.pbnkpk6.mongodb.net/?retryWrites=true&w=majority"
-)
-db = client.dbsparta
+db = client["dbsparta"]
 app = Flask(__name__)
 
 
@@ -26,24 +21,37 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    # sample_receive = request.form['sample_give']
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
-    doc = {
-        "name": name_receive,
-        "comment": comment_receive,
-    }
-    db.fanmessages.insert_one(doc)
-    return jsonify({"msg": "POST request!"})
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form["bucket_give"]
+    count = db.bucket.count_documents({})
+    num = count + 1
+    doc = {"num": num, "bucket": bucket_receive, "done": 0}
+    db.bucket.insert_one(doc)
+    return jsonify({"msg": "data saved!"})
 
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {"_id": False}))
-    return jsonify({"messages": message_list})
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one({"num": int(num_receive)}, {"$set": {"done": 1}})
+    return jsonify({"msg": "Update done!"})
 
 
-if __name__ == "__main__":
+@app.route("/bucket/delete", methods=["POST"])
+def bucket_delete():
+    num_receive = request.form["num_give"]
+    db.bucket.delete_one(
+        {"num": int(num_receive)},
+    )
+    return jsonify({"msg": "delete done!"})
+
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({}, {"_id": False}))
+    return jsonify({"buckets": buckets_list})
+
+
+if __name__ == "_main_":
     app.run("0.0.0.0", port=5000, debug=True)
